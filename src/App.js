af7a1554f6d92db9aff0c235e94587deeb2c927f
Tiago de Oliveira <135615203+TiagoCoder2023@users.noreply.github.com>
2025-09-04 23:02:06 -0300

    Caminhos relativos para imagens

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Timer from "./components/Timer";
 
 function App() {
   const [polaroids, setPolaroids] = useState([
-    { id: 1, image: "/site-polaroids-matheus-mariana/images/foto1.jpeg", text: "", flipped: false },
-    { id: 2, image: "/site-polaroids-matheus-mariana/images/foto2.jpeg", text: "", flipped: false },
-    { id: 3, image: "/site-polaroids-matheus-mariana/images/foto3.jpeg", text: "", flipped: false },
-    { id: 4, image: "/site-polaroids-matheus-mariana/images/foto4.jpeg", text: "", flipped: false },
-    { id: 5, image: "/site-polaroids-matheus-mariana/images/foto5.jpeg", text: "", flipped: false },
-    { id: 6, image: "/site-polaroids-matheus-mariana/images/foto6.jpeg", text: "", flipped: false },
-    { id: 7, image: "/site-polaroids-matheus-mariana/images/foto7.jpeg", text: "", flipped: false },
-    { id: 8, image: "/site-polaroids-matheus-mariana/images/foto8.jpeg", text: "", flipped: false },
-    { id: 9, image: "/site-polaroids-matheus-mariana/images/foto9.jpeg", text: "", flipped: false },
+    { id: 1, image: "./images/foto1.jpeg", text: "", flipped: false },
+    { id: 2, image: "./images/foto2.jpeg", text: "", flipped: false },
+    { id: 3, image: "./images/foto3.jpeg", text: "", flipped: false },
+    { id: 4, image: "./images/foto4.jpeg", text: "", flipped: false },
+    { id: 5, image: "./images/foto5.jpeg", text: "", flipped: false },
+    { id: 6, image: "./images/foto6.jpeg", text: "", flipped: false },
+    { id: 7, image: "./images/foto7.jpeg", text: "", flipped: false },
+    { id: 8, image: "./images/foto8.jpeg", text: "", flipped: false },
+    { id: 9, image: "./images/foto9.jpeg", text: "", flipped: false },
   ]);
 
   const handlePolaroidClick = (id) => {
